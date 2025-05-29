af087b4ddf52e1850172c014d3cf6543141fe67c
Pavel Egorov <egorov.pavel26@gmail.com>
2025-05-29 20:16:03 +0300

    попровил init

--- src/parser/__init__.py
+++ src/parser/__init__.py
@@ -1,13 +1,8 @@
-from .parser import (  # noqa: F401
-    PARSER_FIELDNAMES,
-    get_users_info,
-    map_user_to_row,
-    save_to_csv,
-)
+from .parser import PARSER_FIELDNAMES, get_users_info, map_user_to_row, save_to_csv
 
 __all__ = [
     "PARSER_FIELDNAMES",
-    "get_users_info",
     "map_user_to_row",
+    "get_users_info",
     "save_to_csv",
 ]
